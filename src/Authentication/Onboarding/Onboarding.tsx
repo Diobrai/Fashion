888c0e97d6d1ecb90c1893023d31d7728176c64d
Diobrai <sogodogobrahima52@gmail.com>
2020-08-16 04:14:20 +0100

    first commit

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {View} from "react-native";
+
+const Onboarding = () => {
+    return (
+        <View style={{ flex:1, backgroundColor: "cyan"}} />
+    );
+}
+export default Onboarding;
